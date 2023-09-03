7a3375353f372459832e5967d156af1494b1c0f1
zhang14386 <1438692163@qq.com>
2023-09-03 22:37:47 +0800

    10.2.c提交

---
+++ example/C/10/10.2/10.2.c
@@ -0,0 +1,15 @@
+/*no_data.c--未初始化数组*/
+#include <stdio.h>
+int main(void)
+{
+    /*
+    int no_data[SIZE];  //未初始化数组
+    int i;
+
+    printf("%2s%14s\n",     "i", "no_data");
+    for(i = 0;i < SIZE;i++)
+        printf("%2d%14d\n", i, no_data);
+    */
+   
+    return 0;
+}
