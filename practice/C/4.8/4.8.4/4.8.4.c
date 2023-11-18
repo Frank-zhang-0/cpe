e3dcc3d2468a087928a4ddc7088313deb7e44ec7
zhang14386 <1438692163@qq.com>
2023-11-18 16:21:33 +0800

    4.8.4.c提交

---
+++ practice/C/4.8/4.8.4/4.8.4.c
@@ -0,0 +1,18 @@
+/*
+ *编写一个程序，提示用户输入身高（单位：英寸）和姓名，
+ *然后以下面的格式显示用户刚输入的信息：
+ *Dabney, you are 6.208 feet tall
+ *使用float类型，并用/作为除号。如果你愿意，可以要求用户以厘米为单位输入身高，并以米为单位显示出来。
+*/
+#include <stdio.h>
+int main()
+{
+    char name[40];
+    float tall;
+
+    printf("请输入您的姓名和身高（单位：厘米）：");
+    scanf("%s %f", name, &tall);
+    printf("%s, you are %.3f metre tall\n", name, tall/100);
+
+    return 0;
+}
